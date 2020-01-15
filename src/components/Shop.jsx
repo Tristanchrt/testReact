7d0c417b67ca25eb7903c2ab84bfb6b78c1214b7
Tristancht <tristan@hemka.com>
2020-01-15 17:59:16 +0100

    test redux

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../action';
 
+const Shop = () => {
 
-const Shop = () => (
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
 
+  return (
   <div>
     <h1> Shop page </h1>
-    <h2>Counter</h2>
+    <h2>Counter {counter}</h2>
+    <button onClick={ () => dispatch(increment()) }>+</button>
+    <button  onClick={ () => dispatch(decrement()) }>-</button>
+    {isLogged ? <h3> Uniquememt co jpeux voire</h3> : ''}
   </div>
-);
+  );
+}
 
 export default Shop;
--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -3,9 +3,9 @@ const counterReducer = (state = 0, action) => {
 
     switch(action.type){
         case 'INCREMENT':
-            return + 1;
+            return state + 1;
         case 'DECREMENT':
-            return -1
+            return state - 1;
         default:
             return state
     }
